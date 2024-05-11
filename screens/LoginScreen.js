5a5b5e6ea4f8e0253edc2920f4edf4117287fad4
rafael zanatta <rafaelzanatta@gmail.com>
2024-05-11 15:48:09 -0300

    adiciona animacao

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,6 +1,7 @@
 import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
+import Animated, { FadeIn, FadeInDown, FadeInUp, FadeOut } from 'react-native-reanimated'
 
 export default function LoginScreen() {
     return (
@@ -8,48 +9,39 @@ export default function LoginScreen() {
             className="bg-white h-full w-full"
         >
             <StatusBar style='light' />
-            <Image
-                className="h-full w-full absolute"
-                source={require("../assets/images/background.png")}
-            />
+            <Image className="h-full w-full absolute" source={require("../assets/images/background.png")} />
 
             <View className="flex-row justify-around w-full absolute" >
-                <Image
-                    className="h-[255] w-[90]"
-                    source={require("../assets/images/light.png")}
-                />
-                <Image
-                    className="h-[160] w-[65]"
-                    source={require("../assets/images/light.png")}
-                />
+                <Animated.Image entering={FadeInUp.delay(200).duration(1000).springify()} className="h-[255] w-[90]" source={require("../assets/images/light.png")} />
+                <Animated.Image entering={FadeInUp.delay(400).duration(1000).springify()} className="h-[160] w-[65]" source={require("../assets/images/light.png")} />
             </View>
 
             <View className="h-full w-full flex justify-around pt-40 pb-10">
                 <View className="flex items-center">
-                    <Text className="text-white font-bold tracking-wide text-5xl">
+                    <Animated.Text entering={FadeInUp.duration(1000).springify()} className="text-white font-bold tracking-wide text-5xl">
                         Login
-                    </Text>
+                    </Animated.Text>
                 </View>
 
                 <View className="flex items-center mx-4 space-y-4">
-                    <View className="bg-black/5 p-5 rounded-2xl w-full">
+                    <Animated.View entering={FadeInDown.duration(1000).springify()} className="bg-black/5 p-5 rounded-2xl w-full">
                         <TextInput placeholder="Email" placeholderTextColor={"gray"} />
-                    </View>
+                    </Animated.View>
 
-                    <View className="bg-black/5 p-5 rounded-2xl w-full mb-3">
+                    <Animated.View entering={FadeInDown.delay(200).duration(1000).springify()} className="bg-black/5 p-5 rounded-2xl w-full mb-3">
                         <TextInput placeholder="Password" placeholderTextColor={"gray"} secureTextEntry />
-                    </View>
-                    <View className="w-full">
+                    </Animated.View>
+                    <Animated.View entering={FadeInDown.delay(400).duration(1000).springify()} className="w-full">
                         <TouchableOpacity className="w-full bg-sky-400 p-3 rounded-2xl mb-3">
                             <Text className="text-xl font-bold text-white text-center">Login</Text>
                         </TouchableOpacity>
-                    </View>
-                    <View className="flex-row justify-center gap-x-1">
+                    </Animated.View>
+                    <Animated.View entering={FadeInDown.delay(600).duration(1000).springify()} className="flex-row justify-center gap-x-1">
                         <Text>Don't have an account?</Text>
                         <TouchableOpacity>
                             <Text className="text-sky-600">SignUp</Text>
                         </TouchableOpacity>
-                    </View>
+                    </Animated.View>
                 </View>
             </View>
         </View>
